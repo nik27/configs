44716490cfb42a227e003f8e6fe7d96394faebe3
nik27 <nik27@protonmail.com>
2020-08-12 09:49:47 +0200

    Fix file paths

--- theme-changer.sh
+++ theme-changer.sh
@@ -6,18 +6,19 @@ PID=$(pgrep gnome-session | tail -n1)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
 alacritty=~/.config/alacritty
+custom_scripts=~/custom-scripts
 
-configure_theme() {
+_configure_theme() {
    currenttime=$(date +%H:%M)
    if [[ "$currenttime" > "18:00" ]] || [[ "$currenttime" < "06:00" ]]; then
-    cp ${alacritty}/dark.yml ${alacritty}/alacritty.yml
+    cp ${custom_scripts}/dark.yml ${alacritty}/alacritty.yml
     gsettings set org.gnome.shell.extensions.user-theme name "Mojave-dark"
     gsettings set org.gnome.desktop.interface gtk-theme "Mojave-dark"
    else
-    cp ${alacritty}/light.yml ${alacritty}/alacritty.yml
+    cp ${custom_scripts}/light.yml ${alacritty}/alacritty.yml
     gsettings set org.gnome.shell.extensions.user-theme name "Mojave-light"
     gsettings set org.gnome.desktop.interface gtk-theme "Mojave-light"
    fi
 }
 
-configure_theme
+_configure_theme
